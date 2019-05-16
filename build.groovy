d4bac5c58c7b0e9d411b6abb80c136b1cdbb1dd8
Mohinder Kumar <mohkumar1@publicisgroupe.net>
2019-05-16 18:14:30 +0530

    New

---
+++ build.groovy
@@ -0,0 +1,15 @@
+node ('slave') {
+stage ('Clone') {
+	git url: 'https://github.com/jenkinsorganize/mytest.git'
+}
+stage('commit') {
+	withCredentials([usernamePassword(credentialsId: 'GitHub', passwordVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]) { 
+	sh '''
+	echo hello >> README
+	git add .
+	git commit -m "README"
+	git push --set-upstream https://${GIT_USER}:${GIT_PASS}@github.com/jenkinsorganize/mytest.git master
+	'''
+	}
+}
+}
